07faf3f3290f073f16b2995facdcdda24dcd6286
Jeffrey Warren <jeff@unterbahn.com>
2013-02-04 17:48:49 -0500

    more stylin

--- www/js/application.js
+++ www/js/application.js
@@ -4,7 +4,7 @@ $M = {
   },
   save: function(r) {
     $('#text').val(r)
-    $('img#captured').src = r
+    $('img#captured').src = "data:image/jpeg;base64," + r
     $('img#captured').show()
   }
 }
